e0f686b4a5b66f696758981942090f77249ebab0
Rajabu Shabani <rajabujshabani@gmail.com>
2025-08-12 14:53:15 +0300

    Initialize project

---
+++ src/main/java/com/suineg/onlinecourseplatform/OnlineCoursePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.suineg.onlinecourseplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineCoursePlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineCoursePlatformApplication.class, args);
+    }
+
+}
